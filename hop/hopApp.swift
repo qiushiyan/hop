544f3bbff6c3c3389ade80d180af40e1a7b0a7ee
yanqiushi-mw <qiushi.yann@gmail.com>
2026-01-10 21:01:00 -0600

    Initial Commit

---
+++ hop/hopApp.swift
@@ -0,0 +1,17 @@
+//
+//  hopApp.swift
+//  hop
+//
+//  Created by qiushi on 2026-01-10 21:00.
+//
+
+import SwiftUI
+
+@main
+struct hopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
